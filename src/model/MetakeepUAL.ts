c788b740f70d686049a59591453f8104fe912289
Jose Maria Gayosso <josemaria@hashed.io>
2024-03-02 01:06:46 -0600

    🚀 chore(package.json): add vue dependency to support Vue.js in the project 🔨 refactor(MetakeepUAL.ts): update import statement for AccountCreation component to use relative path 🔨 refactor(MetakeepAuthenticator): improve modal creation for account creation 🔨 refactor(MetakeepAuthenticator): update error handling to include additional message information
    ✨ feat(account-creation.vue): add account creation modal component with form fields and buttons
    🔧 chore(account-creation.vue): refactor styles for better readability and user experience

--- src/model/MetakeepUAL.ts
+++ src/model/MetakeepUAL.ts
@@ -15,6 +15,10 @@ const { metakeepCache } = require('./utils/metakeep-cache');
 const metakeep_name = 'metakeep.ual';
 const Logo = 'data:image/svg+xml,%3C%3Fxml version=\'1.0\' %3F%3E%3Csvg height=\'24\' version=\'1.1\' width=\'24\' xmlns=\'http://www.w3.org/2000/svg\' xmlns:cc=\'http://creativecommons.org/ns%23\' xmlns:dc=\'http://purl.org/dc/elements/1.1/\' xmlns:rdf=\'http://www.w3.org/1999/02/22-rdf-syntax-ns%23\'%3E%3Cg transform=\'translate(0 -1028.4)\'%3E%3Cpath d=\'m3 1030.4c-1.1046 0-2 0.9-2 2v7 2 7c0 1.1 0.8954 2 2 2h9 9c1.105 0 2-0.9 2-2v-7-2-7c0-1.1-0.895-2-2-2h-9-9z\' fill=\'%232c3e50\'/%3E%3Cpath d=\'m3 1049.4c-1.1046 0-2-0.9-2-2v-7-2-3h22v3 2 7c0 1.1-0.895 2-2 2h-9-9z\' fill=\'%2334495e\'/%3E%3Cpath d=\'m4 1032.9v1.1l2 2.4-2 2.3v1.1l3-3.4-3-3.5z\' fill=\'%23ecf0f1\'/%3E%3Cpath d=\'m3 2c-1.1046 0-2 0.8954-2 2v7 2 3h22v-3-2-7c0-1.1046-0.895-2-2-2h-9-9z\' fill=\'%2334495e\' transform=\'translate(0 1028.4)\'/%3E%3Cpath d=\'m4 5.125v1.125l3 1.75-3 1.75v1.125l5-2.875-5-2.875zm5 4.875v1h5v-1h-5z\' fill=\'%23ecf0f1\' transform=\'translate(0 1028.4)\'/%3E%3C/g%3E%3C/svg%3E';
 
+const { createApp, ref } = require('vue');
+// const AccountCreation = require('utils/account-creation.vue').default
+const AccountCreation = require('./utils/account-creation').default
+
 let metakeep
 class MetakeepUser extends User {
     constructor({
@@ -310,16 +314,33 @@ class MetakeepAuthenticator extends Authenticator {
 
     async createAccount(publicKey) {
         try {
-            const res = await axios.post(this.accountCreateAPI, {
-                ownerKey: publicKey,
-                activeKey: publicKey,
-                // jwt: this.userCredentials.jwt,
-                // suggestedName: 'somevalidname', // we are not using this optional parameter for now
+            const modalContainer = document.getElementById('c-app')
+            console.log("modalContainer", modalContainer)
+
+            return new Promise((approve, reject) => {
+                const app = createApp(AccountCreation, {
+                    onCancel: () => {
+                      reject("User closed account creation dialog.")
+                    },
+                    onCreateAccount: (data) => {
+                        console.log('Evento recibido: onCreateAccount: ', data);
+                    }
+                  })
+                console.log("Creating modal", app)
+                const vm = app.mount(modalContainer);
             })
+
+
+            // const res = await axios.post(this.accountCreateAPI, {
+            //     ownerKey: publicKey,
+            //     activeKey: publicKey,
+            //     // jwt: this.userCredentials.jwt,
+            //     // suggestedName: 'somevalidname', // we are not using this optional parameter for now
+            // })
             // .then(response => response.data.accountName);
             // debugger
-            console.log("create account response: ", res)
-            return res.data.accountName
+            // console.log("create account response: ", res)
+            // return res.data.accountName
         } catch (e) {
             console.error("Error trying to create account, please try again.", e);
             throw new Error("Error trying to create account, please try again.")
@@ -341,7 +362,7 @@ class MetakeepAuthenticator extends Authenticator {
 
             // we check if we have the account name in the cache
             const accountNames = metakeepCache.getAccountNames(this.userCredentials.email, this.chainId);
-            if (accountNames.length > 0) {
+            if (accountNames.length > 0 && accountNames[0] != null) {
                 resolve(accountNames[0]);
                 return;
             }
@@ -363,7 +384,6 @@ class MetakeepAuthenticator extends Authenticator {
                 if (accountExists) {
                     accountName = response.data.account_names[0];
                 } else {
-                    // console.log('vamos a crear la cuenta');
                     accountName = await this.createAccount(publicKey);
                 }
 
@@ -425,8 +445,9 @@ class MetakeepAuthenticator extends Authenticator {
             return [userInstance];
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
         } catch (err) {
+            
             this.loading = false;
-            let message = (err.response?.data || err.message)
+            let message = (err.response?.data || err.message || err)
             throw new UALError(message, UALErrorType.Login, err, 'MetakeepAuthenticator');
         }
     };
---
+++ src/model/utils/account-creation.vue
@@ -0,0 +1,200 @@
+<template>
+<div>
+  <div class="cModal" v-if="showModal == true">
+    <div class="cModal-content">
+        <h4 class="cTitle" v-if="isLoading == false">Account creation</h4>
+        <div v-if="isLoading == false">
+          <label for="accountName" class="label">Telos account</label>
+          <br>
+          <input v-model="telosAccountName" id="accountName" class="cInput" autocomplete="off">
+          <p class="cErrorLabel"> This is an error message</p>
+          <br>
+          <div class="cButtonsContainer">
+            <button class="cButton cancelButton" @click="closeModal">Cancel</button>
+            <button
+                class="cButton approveButton"
+                @click="createAccount"
+            >Create account</button>
+          </div>
+       
+        </div>
+        <div v-else class="cCenter-container">
+          <div>
+            <div class="cRow ">
+              <div class="cCenter-container">
+                <p>Wait a moment</p>
+              </div>
+            </div>
+            <div class="cRow">
+              <div class="cCenter-container">
+                <div class="cSpinner "></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+  </div>
+</div>
+</div>
+</template>
+
+<script setup>
+import { onMounted, ref, defineProps, computed } from 'vue'
+
+const showModal = ref(false)
+const isLoading = ref(false)
+const telosAccountName = ref(undefined)
+const props = defineProps(['onCancel', 'onCreateAccount'])
+
+
+onMounted(() => {
+    showModal.value = true
+})
+
+const isCreateButtonDisabled = computed(() => {
+    return telosAccountName.value?.length === 0
+})
+
+const closeModal = () => {
+    showModal.value = false
+    props.onCancel()
+}
+
+function createAccount () {
+    validateAccountName()
+    props.onCreateAccount(telosAccountName.value)
+}
+
+const apiURL = "https://telos-account-creator-test-c1d9d1af38a4.herokuapp.com/v1"
+
+async function validateAccountName () {
+    try {
+        const endpointURL = `${apiURL}/accounts/${telosAccountName.value}`
+        console.log("endpointURL", endpointURL)
+        isLoading.value = true
+        const response = await fetch(endpointURL)
+        console.log("response", response)
+    } catch (e) {
+    } finally {
+        isLoading.value = false
+    }
+}
+
+</script>
+
+<style>
+.cErrorLabel {
+  color: #f44336;
+  padding: 0px;
+  margin: 0px;
+}
+
+.cInput {
+  padding: 15px;
+  width: 94%;
+  border-radius: 8px;
+  background-color: #f2f2f2;
+  border: 1px solid #ccc;
+  outline: none;
+  transition: border-color 0.3s ease;
+}
+
+.cInput:focus {
+  border-color: #2196F3; /* Cambia el color del borde al enfocar */
+}
+
+.cButtonsContainer {
+  display: flex;
+  justify-content: space-between; /* Cambiado a space-between para distribuir uniformemente */
+  margin-top: 0px; /* Agregado margen superior */
+}
+
+.cLabel {
+  color: #666; /* Cambiado a un tono más oscuro */
+}
+
+.cTitle {
+  color: #333; /* Cambiado a un tono más oscuro */
+  font-size: 1.2rem; /* Aumentado el tamaño de fuente */
+  margin-bottom: 20px; /* Agregado margen inferior */
+}
+
+.approveButton, .cancelButton {
+  padding: 12px 20px; /* Ajuste del tamaño de los botones */
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease, color 0.3s ease; /* Transición suave */
+}
+
+.approveButton {
+  background-color: #4CAF50; /* Color verde */
+  color: white;
+}
+
+.approveButton:hover {
+  background-color: #45a049; /* Color verde oscuro al pasar el ratón */
+}
+
+.cancelButton {
+  background-color: #f44336; /* Color rojo */
+  color: white;
+}
+
+.cancelButton:hover {
+  background-color: #d32f2f; /* Color rojo oscuro al pasar el ratón */
+}
+
+.cButton {
+  width: 48%; /* Ligeramente reducido el ancho para ajustar mejor */
+}
+
+.cModal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.cModal-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Sombra suave */
+  width: 400px;
+}
+
+.cSpinner {
+  width: 50px;
+  height: 50px;
+  border: 3px solid rgba(0, 0, 0, 0.1);
+  border-top-color: #333;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+.cCenter-container {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
+
+.c_row {
+  display: flex; /* Hace que los elementos hijos se alineen en una fila */
+  flex-wrap: wrap; /* Permite que los elementos hijos se envuelvan a la siguiente línea si no hay suficiente espacio */
+  margin-right: -15px; /* Corrige el margen derecho negativo generado por el uso de flexbox */
+  margin-left: -15px; /* Corrige el margen izquierdo negativo generado por el uso de flexbox */
+}
+
+</style>
